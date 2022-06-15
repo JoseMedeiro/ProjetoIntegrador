25009b8e789a0f237aa9252dc4f64094b80893c3
RafaelaChaffilla <rafaela.chaffilla@tecnico.ulisboa.pt>
2022-06-15 12:58:32 +0100

    novo cm e ponto neutro
    temos estabilidade forçada, mas temos

--- aircraft-design-tool-main/landingGear.m
+++ aircraft-design-tool-main/landingGear.m
@@ -20,3 +20,42 @@ DwheelNL = Dwheel*0.4
 LwheelNL = Lwheel*0.4
 LwheelNLTotal = Nrodas/2*LwheelNL
 volume2= LwheelNLTotal*(DwheelNL/2)^2*pi/1000000
+
+
+%% ponto neutro 
+
+%pelo azores vtol:
+
+%wing
+c=2;                % corda   
+r= 0.7;             % posição da asa em relação ao inicio da fuselagem
+wp = 1.5 + r;       % posição da asa
+xw= wp + 0.25*c;   % considerando 0.25 de corda como o ponto aerodinamico
+aw=0.106; % [/º]
+Sw=30;    % [m^2]
+lht=(1 - r) + 3;  % [m]
+at=aw;
+St=Sw;
+e_alfa = 0.4;
+%xpn=(xw*aw*Sw + (xw+lht)*at*St)/(at*St+aw*Sw)
+
+%pela aula 11 só dá o centro aerodinamico da asa da frente :
+% confirma-se os calculos
+%wing
+hnw=0.25; % xfoil considera 1/4 de corda como o ponto aerodinamico
+
+hn=(hnw+(lht/c + hnw)*(1-e_alfa))/(1+1)
+xpn=wp+hn*2
+
+xcg = 4.37
+SM=(xpn - xcg)/c
+
+% cm_alfa
+h= (xcg - 1.9)/c;
+hnw = 0.25;
+ht=hnw;
+
+cm_alfa  = (aw*(xcg - xw) - (lht + xw - xcg)*at)/c*(1-e_alfa);
+%confirmação
+cl_alfa  = aw+at*(1-e_alfa);
+cm_alfa1 = -cl_alfa*SM
